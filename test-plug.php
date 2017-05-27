576b8bdcb0e86c0d51bc5e6dcf9f38756211b25d
snehawalunje <sneha.khare047@gmail.com>
2017-05-27 19:21:39 +0530

    change description

---
+++ test-plug.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Test Plug
+ * Plugin URI: http://testplug.com
+ * Author: Sneha Walunje
+ * Author URI: http://testplug.com
+ * Version: 1.0.0
+ * Description: This is my first practise plugin to learn plugin development.
+**/
+?>
