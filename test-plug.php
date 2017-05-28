558b7cb8fc0d8395aab9444341976968d11e7602
snehawalunje <sneha.khare047@gmail.com>
2017-05-28 15:40:51 +0530

    change  author url

--- test-plug.php
+++ test-plug.php
@@ -3,7 +3,7 @@
  * Plugin Name: Test Plug
  * Plugin URI: http://testplug.com
  * Author: Sneha Walunje
- * Author URI: http://testplug.com
+ * Author URI: http://wpshopee.com
  * Version: 1.0.0
  * Description: This is my first practise plugin to learn plugin development.
 **/
